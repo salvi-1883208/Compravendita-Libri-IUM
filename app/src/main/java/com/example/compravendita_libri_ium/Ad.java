5989b7c964f68e9d8656c0ef999828be26a8dcf4
Marco <salvi.1883208@studenti.uniroma1.it>
2022-01-21 11:19:58 +0100

    Added Order class and AdBase class, Orders enum and Seller class

--- app/src/main/java/com/example/compravendita_libri_ium/Ad.java
+++ app/src/main/java/com/example/compravendita_libri_ium/Ad.java
@@ -1,63 +1,24 @@
 package com.example.compravendita_libri_ium;
 
-import android.media.Image;
-
-import java.util.Arrays;
-
 public class Ad {
     private boolean approved = false;
-    private boolean completed = false;
-    private UsedBook book;
-    private double price;
-    private int[] photosId;
-    private MeetingPlace meetingPlace;
+    private AdBase adBase;
 
-    public Ad(UsedBook book, double price, int[] photosId, MeetingPlace meetingPlace) {
-        this.book = book;
-        this.price = price;
-        this.photosId = photosId;
-        this.meetingPlace = meetingPlace;
+    public Ad(AdBase adBase, boolean approved) {
+        this.adBase = adBase;
+        this.approved = approved;
     }
 
-    public UsedBook getBook() {
-        return book;
+    public AdBase getAdBase() {
+        return adBase;
     }
 
     public boolean isApproved() {
         return approved;
     }
 
-    public double getPrice() {
-        return price;
-    }
-
-    public int[] getPhotos() {
-        return photosId;
-    }
-
-    public MeetingPlace getMeetingPlace() {
-        return meetingPlace;
-    }
-
     public void approve() {
         approved = true;
     }
 
-    public void complete() throws Exception {
-        if (approved)
-            completed = true;
-        else
-            throw new Exception("You can't complete an order if the ad is not approved yet");
-    }
-
-
-    @Override
-    public String toString() {
-        return "Ad{" +
-                "approved=" + approved +
-                ", book=" + book +
-                ", price=" + price +
-                ", meetingPlace=" + meetingPlace +
-                '}';
-    }
 }
---
+++ app/src/main/java/com/example/compravendita_libri_ium/AdBase.java
@@ -0,0 +1,44 @@
+package com.example.compravendita_libri_ium;
+
+import android.media.Image;
+
+import java.util.Arrays;
+
+public class AdBase {
+    private UsedBook book;
+    private double price;
+    private int[] photosId;
+    private MeetingPlace meetingPlace;
+
+    public AdBase(UsedBook book, double price, int[] photosId, MeetingPlace meetingPlace) {
+        this.book = book;
+        this.price = price;
+        this.photosId = photosId;
+        this.meetingPlace = meetingPlace;
+    }
+
+    public UsedBook getBook() {
+        return book;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int[] getPhotos() {
+        return photosId;
+    }
+
+    public MeetingPlace getMeetingPlace() {
+        return meetingPlace;
+    }
+
+    @Override
+    public String toString() {
+        return "AdBase{" +
+                ", book=" + book +
+                ", price=" + price +
+                ", meetingPlace=" + meetingPlace +
+                '}';
+    }
+}
