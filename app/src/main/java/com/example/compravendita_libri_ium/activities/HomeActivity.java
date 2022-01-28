d52c16cfff10ed520d482aef4a59a43736a62973
ANDREA\donel <donelliandrea2000@gmail.com>
2022-01-28 16:58:38 +0100

    x

--- app/src/main/java/com/example/compravendita_libri_ium/activities/HomeActivity.java
+++ app/src/main/java/com/example/compravendita_libri_ium/activities/HomeActivity.java
@@ -5,11 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.LinearLayout;
 
 import com.example.compravendita_libri_ium.R;
-import com.example.compravendita_libri_ium.sell_activity;
 
 public class HomeActivity extends AppCompatActivity {
 
