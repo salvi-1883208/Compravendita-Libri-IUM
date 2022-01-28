8ad2305155b2e26ab56b338600d10a6e460712c2
Marco <salvi.1883208@studenti.uniroma1.it>
2022-01-28 18:11:38 +0100

    Update HomeActivity to use bindings and deleted old sell_activity and its xml

--- app/src/main/java/com/example/compravendita_libri_ium/activities/HomeActivity.java
+++ app/src/main/java/com/example/compravendita_libri_ium/activities/HomeActivity.java
@@ -4,63 +4,37 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.LinearLayout;
 
-import com.example.compravendita_libri_ium.R;
-import com.example.compravendita_libri_ium.sell_activity;
+import com.example.compravendita_libri_ium.databinding.ActivityHomeBinding;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private ActivityHomeBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         getSupportActionBar().setTitle("Sezione compravendita libri");
 
-        LinearLayout vendi_libro = findViewById(R.id.vendi_libri);
-        vendi_libro.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent OpenVendita = new Intent(HomeActivity.this, sell_activity.class);
-                startActivity(OpenVendita);
-
-            }
+        binding.vendiLibri.setOnClickListener(view -> {
+            Intent OpenVendita = new Intent(HomeActivity.this, NewAdActivity.class);
+            startActivity(OpenVendita);
         });
 
-        LinearLayout compra_libro = findViewById(R.id.compra_libri);
-        compra_libro.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        binding.compraLibri.setOnClickListener(view -> {
 
-            }
         });
 
-        LinearLayout ordini_effettuati = findViewById(R.id.ordini_effettuati);
-        ordini_effettuati.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent OpenOrdini = new Intent(HomeActivity.this,OrdersActivity.class);
-                startActivity(OpenOrdini);
-            }
+        binding.ordiniEffettuati.setOnClickListener(view -> {
+            Intent OpenOrdini = new Intent(HomeActivity.this, OrdersActivity.class);
+            startActivity(OpenOrdini);
         });
 
-        LinearLayout annunci_inseriti = findViewById(R.id.annunci_inseriti);
-        annunci_inseriti.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent OpenAnnunci = new Intent(HomeActivity.this, AdsActivity.class);
-                startActivity(OpenAnnunci);
-            }
-        });
-
-        LinearLayout profilo_utente = findViewById(R.id.profilo_utente);
-        profilo_utente.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
+        binding.annunciInseriti.setOnClickListener(view -> {
+            Intent OpenAnnunci = new Intent(HomeActivity.this, AdsActivity.class);
+            startActivity(OpenAnnunci);
         });
     }
 }
