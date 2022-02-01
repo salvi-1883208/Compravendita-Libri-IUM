097757092c7ee0b7419d5d6e7db78c2e9c6147c3
Marco <salvi.1883208@studenti.uniroma1.it>
2022-02-01 18:07:41 +0100

    Added MyAds class

---
+++ app/src/main/java/com/example/compravendita_libri_ium/MyAds.java
@@ -0,0 +1,23 @@
+package com.example.compravendita_libri_ium;
+
+import android.net.Uri;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public enum MyAds {
+    FISICA_I_2008(new Ad(new AdBase(Books.FISICA_I_2008.getBook().toUsedBook(BookCondition.DISCRETE_CONDIZIONI, new ArrayList<>(Arrays.asList(BookSubCondition.SOTT_MATITA_O_PENNA))),
+            35.0, new ArrayList<Uri>(Arrays.asList(AdBase.drawableToUri(R.drawable.lezioni_di_fisica_front), AdBase.drawableToUri(R.drawable.lezioni_di_fisica_back))), MeetingPlace.FERMI), false)),
+    FISICA_II_2008(new Ad(new AdBase(Books.FISICA_II_2008.getBook().toUsedBook(BookCondition.DISCRETE_CONDIZIONI, new ArrayList<>(Arrays.asList(BookSubCondition.SOTT_EVIDENZ))),
+            30.0, new ArrayList<Uri>(Arrays.asList(AdBase.drawableToUri(R.drawable.lezioni_di_fisica_ii_0), AdBase.drawableToUri(R.drawable.lezioni_di_fisica_ii_1))), MeetingPlace.FERMI), true));
+    private Ad ad;
+
+    MyAds(Ad ad) {
+        this.ad = ad;
+    }
+
+    public Ad getAd() {
+        return ad;
+    }
+
+}
