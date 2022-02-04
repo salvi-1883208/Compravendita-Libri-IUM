46c85e8af81d4b4e167f4106af0b6ebadcca41e2
Marco <salvi.1883208@studenti.uniroma1.it>
2022-02-04 15:19:39 +0100

    Added more books to be available for sale and fixed continua button showing in activity_new_ad_meeting_place.xml

--- app/src/main/java/com/example/compravendita_libri_ium/BooksToSell.java
+++ app/src/main/java/com/example/compravendita_libri_ium/BooksToSell.java
@@ -11,6 +11,10 @@ public class BooksToSell {
 
     // Books to sell (the books you choose and then create an ad for adding the conditions...)
     private BooksToSell() {
+        books.add(Books.ALGORITMI_STR_DAT.getBook());
+        books.add(Books.ANALISI_MAT.getBook());
+        books.add(Books.ANALISI_MAT_II.getBook());
+        books.add(Books.ELEMENTI_FISICA.getBook());
         books.add(Books.FISICA_I_2008.getBook());
         books.add(Books.FISICA_I_2011.getBook());
         books.add(Books.FISICA_II_2008.getBook());
