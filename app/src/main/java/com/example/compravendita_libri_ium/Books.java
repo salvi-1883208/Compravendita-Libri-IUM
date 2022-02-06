f45803385108ca5889f1440b6d7d0a2fa17dd7bb
Marco <salvi.1883208@studenti.uniroma1.it>
2022-02-06 19:41:49 +0100

    Corrected ANALISI_MAT author name

--- app/src/main/java/com/example/compravendita_libri_ium/Books.java
+++ app/src/main/java/com/example/compravendita_libri_ium/Books.java
@@ -6,7 +6,7 @@ public enum Books {
     ELEMENTI_FISICA(new Book("Elementi di Fisica", 2008, "Claudio Gialli", "Cittàstudi", "8737592865", "Fisica")),
     FISICA_I_2008(new Book("Lezioni di Fisica I", 2008, "Mario Rossi", "Zanichelli", "0123456789", "Fisica")),
     FISICA_I_2011(new Book("Lezioni di Fisica I", 2011, "Mario Rossi", "Zanichelli", "10234789456", "Fisica")),
-    ANALISI_MAT(new Book("Analisi Matematica I", 2005, "Rossi", "Zanichelli", "0864297531", "Algebra")),
+    ANALISI_MAT(new Book("Analisi Matematica I", 2005, "Claudio Rossi", "Zanichelli", "0864297531", "Algebra")),
     FISICA_II_2008(new Book("Lezioni di Fisica II", 2008, "Mario Rossi", "Zanichelli", "098721452", "Fisica")),
     FISICA_II_2011(new Book("Lezioni di Fisica II", 2011, "Mario Rossi", "Zanichelli", "3215678900", "Fisica")),
     ANALISI_MAT_II(new Book("Analisi Matematica II", 2011, "Gianni Gialli", "Zanichelli", "29638729479", "Algebra"));
